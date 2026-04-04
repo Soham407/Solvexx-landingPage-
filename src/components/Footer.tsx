f4fc39d7f47f1bb2a9213e2ba6a2ab0e414c21fd
Soham407 <sohambhutkar1@gmail.com>
2026-04-04 14:47:29 +0530

    trying new things

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Mail, Phone, MapPin, Facebook, Linkedin, Twitter } from 'lucide-react';
-import Logo from '../assets/Logo.png';
+import Logo from '/Logo.png';
 
 const Footer = () => {
   return (
